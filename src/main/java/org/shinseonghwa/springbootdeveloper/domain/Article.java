b84d00cb581e813e5b93c355ba5f6df0feffdbc6
seonghwa95 <holyflower95@naver.com>
2023-09-27 22:35:57 +0900

    Article Entity annotation으로 변경

--- src/main/java/org/shinseonghwa/springbootdeveloper/domain/Article.java
+++ src/main/java/org/shinseonghwa/springbootdeveloper/domain/Article.java
@@ -1,11 +1,15 @@
 package org.shinseonghwa.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
@@ -22,23 +26,5 @@ public class Article {
     public Article(String title, String content) {
         this.title = title;
         this.content = content;
-    }
-
-    // 기본 생성자
-    public Article() {
-
-    }
-
-    // Getter
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
     }
 }
