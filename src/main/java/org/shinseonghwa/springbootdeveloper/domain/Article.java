d2eccf20e8bc75e93d30d04ca8376a3f9bc110a2
seonghwa95 <holyflower95@naver.com>
2023-10-05 19:57:58 +0900

    Blog View 진행중..

--- src/main/java/org/shinseonghwa/springbootdeveloper/domain/Article.java
+++ src/main/java/org/shinseonghwa/springbootdeveloper/domain/Article.java
@@ -5,6 +5,11 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,6 +25,14 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     // 빌더 패턴
     @Builder
--- src/main/java/org/shinseonghwa/springbootdeveloper/dto/ArticleViewResponse.java
+++ src/main/java/org/shinseonghwa/springbootdeveloper/dto/ArticleViewResponse.java
@@ -6,15 +6,17 @@ import org.shinseonghwa.springbootdeveloper.domain.Article;
 import java.time.LocalDateTime;
 
 @Getter
-public class ArticleListViewResponse {
+public class ArticleViewResponse {
 
     private final Long id;
     private final String title;
     private final String content;
+    private final LocalDateTime created_at;
 
-    public ArticleListViewResponse(Article article) {
+    public ArticleViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.created_at = article.getCreatedAt();
     }
 }
