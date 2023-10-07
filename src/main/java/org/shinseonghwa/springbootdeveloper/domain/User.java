d163b8d3a8b080381dc166c6ba796c661a73f93d
seonghwa95 <holyflower95@naver.com>
2023-10-08 02:43:20 +0900

    User 도메인, 레포지토리, 서비스

---
+++ src/main/java/org/shinseonghwa/springbootdeveloper/domain/User.java
@@ -0,0 +1,85 @@
+package org.shinseonghwa.springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Builder
+    public User(String email, String password, String auth) {
+        this.email = email;
+        this.password = password;
+    }
+
+
+    // 권한 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    // 사용자의 이름을 반환 (고유 값)
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    // 왜 얘만 자동으로 Override 되지 않았을까? (수동으로 적음)
+    // 사용자의 비밀번호를 반환
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    
+    // 계정 만료 여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        // 만료되었는지 확인하는 로직
+        return true;    // true -> 만료되지 않음
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        // 계정 잠금되었는지 확인하는 로직
+        return true;    // true -> 잠금되지 않음
+    }
+
+    // 패스워드의 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // 패스워드가 만료되었는지 확인하는 로직
+        return true;    // true -> 만료되지 않음
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        // 계정이 사용 가능한지 확인하는 로직
+        return true;    // true -> 사용 가능
+    }
+}
