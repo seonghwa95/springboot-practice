9f894235da27660c9a761e97755313d91627ce5b
seonghwa95 <holyflower95@naver.com>
2023-10-26 10:10:45 +0900

    security Jwt 방식 진행중 Token DTO, Service 추가

---
+++ src/main/java/org/shinseonghwa/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,11 @@
+package org.shinseonghwa.springbootdeveloper.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+
+    private String accessToken;
+}
