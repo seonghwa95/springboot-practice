ed3925ac9b1f32a56528bd018b0a1093e55652bb
seonghwa95 <holyflower95@naver.com>
2023-10-31 02:50:26 +0900

    security Jwt 방식 진행중 CreateAccessTokenRequest 추가 Test 실행

---
+++ src/main/java/org/shinseonghwa/springbootdeveloper/controller/TokenApiController.java
@@ -0,0 +1,26 @@
+package org.shinseonghwa.springbootdeveloper.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.shinseonghwa.springbootdeveloper.dto.CreateAccessTokenRequest;
+import org.shinseonghwa.springbootdeveloper.dto.CreateAccessTokenResponse;
+import org.shinseonghwa.springbootdeveloper.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createAccessToken(@RequestBody CreateAccessTokenRequest request) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
--- src/main/java/org/shinseonghwa/springbootdeveloper/dto/CreateAccessTokenRequest.java
+++ src/main/java/org/shinseonghwa/springbootdeveloper/dto/CreateAccessTokenRequest.java
@@ -7,5 +7,5 @@ import lombok.Setter;
 @Setter
 public class CreateAccessTokenRequest {
 
-    private String accessToken;
+    private String refreshToken;
 }
