88f5899744a666ab8a5ec14c199a7923564fe7fc
seonghwa95 <holyflower95@naver.com>
2023-11-05 01:03:27 +0900

    OAuth2SuccessHandler 작성중

--- src/main/java/org/shinseonghwa/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/org/shinseonghwa/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
@@ -1,16 +1,56 @@
 package org.shinseonghwa.springbootdeveloper.config.oauth;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.shinseonghwa.springbootdeveloper.config.jwt.TokenProvider;
+import org.shinseonghwa.springbootdeveloper.domain.RefreshToken;
+import org.shinseonghwa.springbootdeveloper.domain.User;
 import org.shinseonghwa.springbootdeveloper.repository.RefreshTokenRepository;
 import org.shinseonghwa.springbootdeveloper.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Duration;
 
 @RequiredArgsConstructor
-public class OAuth2SuccessHandler {
+@Component
+public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    public static final Duration REFRESH_TOKEN_DURATION = Duration.ofDays(14);
+    public static final Duration ACCESS_TOKEN_DURATION = Duration.ofDays(1);
+    public static final String REDIRECT_PATH = "/articles";
+
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
     private final OAuth2AuthorizationRequestBasedOnCookieRepository authorizationRequestRepository;
     private final UserService userService;
 
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
 
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        User user = userService.findByEmail((String) oAuth2User.getAttributes().get("email"));
+
+        // 리프레시 토큰 생성 -> 저장 -> 쿠키에 저장
+        String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
+        saveRefreshToken(user.getId(), refreshToken);
+        // -- 여기 부터
+    }
+
+    // 생성된 리프레시 토큰을 전달받아 데이터베이스에 저장
+    private void saveRefreshToken(Long userId, String newRefreshToken) {
+        RefreshToken refreshToken = refreshTokenRepository.findByUserId(userId)
+                .map(entity -> entity.update(newRefreshToken))
+                .orElse(new RefreshToken(userId, newRefreshToken));
+
+        refreshTokenRepository.save(refreshToken);
+    }
+    
+    // 생성된 리프레시 토큰을 쿠키에 저장
+    // 여기 부터
 }
