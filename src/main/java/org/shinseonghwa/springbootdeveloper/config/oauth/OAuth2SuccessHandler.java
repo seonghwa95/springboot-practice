5a4e104d17427182817889b8451323c4f5f3b678
seonghwa95 <holyflower95@naver.com>
2023-11-06 22:46:39 +0900

    OAuth2 로그인 방식 진행중 OAuth2SuccessHandler

--- src/main/java/org/shinseonghwa/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
+++ src/main/java/org/shinseonghwa/springbootdeveloper/config/oauth/OAuth2SuccessHandler.java
@@ -9,10 +9,12 @@ import org.shinseonghwa.springbootdeveloper.domain.RefreshToken;
 import org.shinseonghwa.springbootdeveloper.domain.User;
 import org.shinseonghwa.springbootdeveloper.repository.RefreshTokenRepository;
 import org.shinseonghwa.springbootdeveloper.service.UserService;
+import org.shinseonghwa.springbootdeveloper.util.CookieUtil;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -39,7 +41,14 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         // 리프레시 토큰 생성 -> 저장 -> 쿠키에 저장
         String refreshToken = tokenProvider.generateToken(user, REFRESH_TOKEN_DURATION);
         saveRefreshToken(user.getId(), refreshToken);
-        // -- 여기 부터
+        addRefreshTokenToCookie(request, response, refreshToken);
+        // 액세스 토큰 생성 -> 패스에 액세스 토큰 추가
+        String accessToken = tokenProvider.generateToken(user, ACCESS_TOKEN_DURATION);
+        String targetUrl = getTargetUrl(accessToken);
+        // 인증 관련 설정값, 쿠키 제거
+        clearAuthenticationAtributes(request, response);
+        // 리다이렉트
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
     }
 
     // 생성된 리프레시 토큰을 전달받아 데이터베이스에 저장
@@ -50,7 +59,25 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         refreshTokenRepository.save(refreshToken);
     }
-    
+
     // 생성된 리프레시 토큰을 쿠키에 저장
-    // 여기 부터
+    private void addRefreshTokenToCookie(HttpServletRequest request, HttpServletResponse response, String refreshToken) {
+        int cookieMaxAge = (int) REFRESH_TOKEN_DURATION.toSeconds();
+        CookieUtil.deleteCookie(request, response, REFRESH_TOKEN_COOKIE_NAME);
+        CookieUtil.addCookie(response, REFRESH_TOKEN_COOKIE_NAME, refreshToken, cookieMaxAge);
+    }
+
+    // 인증 관련 설정값, 쿠키 제거
+    private void clearAuthenticationAtributes(HttpServletRequest request, HttpServletResponse response) {
+        super.clearAuthenticationAttributes(request);
+        authorizationRequestRepository.removeAuthorizationRequestCookies(request, response);
+    }
+
+    // 액세스 토큰을 패스에 추가
+    private String getTargetUrl(String token) {
+        return UriComponentsBuilder.fromUriString(REDIRECT_PATH)
+                .queryParam("token", token)
+                .build()
+                .toUriString();
+    }
 }
